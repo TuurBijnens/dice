d792a36e647fc0c31728b8380e1edf07c8098b75
TuurBijnens <117900663+TuurBijnens@users.noreply.github.com>
2022-11-28 11:18:36 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,37 +1,34 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
+        # . # . #
+        . . # . .
         # # # # #
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
+        # . . . #
+        # . . . #
         `)
     basic.showLeds(`
         # . . . #
         # . . . #
-        # . . . #
-        # . . . #
         # # # # #
-        `)
-    basic.showLeds(`
-        # . . . #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showLeds(`
-        # # # # .
-        # . . # .
-        # # # # .
-        # . # . .
-        # . . # .
+        . . # . .
+        # . # . #
         `)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showString("goed gedaan")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . # # # .
+        `)
     basic.showString("nu knop a")
 })
+input.onSound(DetectedSound.Loud, function () {
+    basic.showString("" + (input.temperature()))
+})
+input.setSoundThreshold(SoundThreshold.Loud, 128)
 basic.showLeds(`
     # # . # #
     . # # # .
